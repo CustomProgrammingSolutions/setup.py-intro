b3310629ca949eccaf0efa9c474f6cc7e8eb1f4f
Janis Lesinskis <janis@lesinskis.com>
2018-12-09 20:22:14 +1100

    Example of __all__

--- hello_world/__init__.py
+++ hello_world/__init__.py
@@ -2,5 +2,14 @@
 When you install this package you find the function in this file at hello_world.other_greeting()
 """
 
+
+# defines what is available when we do a `from hello_world import *`
+__all__ = ['other_greeting']
+
 def other_greeting():
     print("Evening comrade")
+
+
+def secret_greeting():
+    """This greeting will only be shown to people that know about it, anyone using a import * will not see this"""
+    print("Very secret greeting!)
